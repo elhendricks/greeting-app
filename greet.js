59f09be00ad9838cfb206b7efaabe510940390eb
elhendricks <elhendricks@gmail.com>
2016-10-17 16:12:17 -0700

    added command line prompt and response

--- greet.js
+++ greet.js
@@ -1,3 +1,15 @@
+var readline = require('readline');
+
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('What is your name?', function(answer) {
+    console.log(greet(answer));
+    rl.close();
+});
+
 function greet(str) {
     var name = str || 'friend';
     return 'hello ' + name;
